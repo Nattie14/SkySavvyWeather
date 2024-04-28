663da48ef55b5def9a245d22d47b5e4b122877b0
Nattie14 <166996757+Nattie14@users.noreply.github.com>
2024-04-28 09:18:52 +0100

    Updated java script to display a 5 day weather forecast

--- src/script.js
+++ src/script.js
@@ -1,60 +1,112 @@
 function refreshWeather(response) {
-    let temperatureElement = document.querySelector("#temperature");
-    let temperature = response.data.temperature.current;
-    let cityElement = document.querySelector("#city");
-    let descriptionElement = document.querySelector("#description");
-    let humidityElement = document.querySelector("#humidity");
-    let windSpeedElement = document.querySelector("#wind-speed");
-    let timeElement = document.querySelector("#time");
-    let date = new Date(response.data.time * 1000);
-    let iconElement = document.querySelector("#icon");
-   
-  
-    temperatureElement.innerHTML = Math.round(temperature);
-    descriptionElement.innerHTML = capitalizeEveryWord(response.data.condition.description);
-    cityElement.innerHTML = response.data.city;
-    humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
-    windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
-    timeElement.innerHTML = formatDate(date);
-    iconElement.innerHTML =  `<img src="${response.data.condition.icon_url}"
-    class="weather-app-icon"/>`
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  temperatureElement.innerHTML = Math.round(temperature);
+  descriptionElement.innerHTML = capitalizeEveryWord(
+    response.data.condition.description
+  );
+  cityElement.innerHTML = response.data.city;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  timeElement.innerHTML = formatDate(date);
+  iconElement.innerHTML = `<img src="${response.data.condition.icon_url}"
+    class="weather-app-icon"/>`;
 }
 
 function capitalizeEveryWord(string) {
-    return string.replace(/\b\w/g, (char) => char.toUpperCase());
+  return string.replace(/\b\w/g, (char) => char.toUpperCase());
 }
 
 function formatDate(date) {
-    let hours = date.getHours();
-    let minutes = date.getMinutes();
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-    let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  
-    let day = days[date.getDay()];
-    let month = months[date.getMonth()];
-  
-    return `${day} ${month} ${date.getDate()}, ${hours}:${minutes < 10 ? '0' + minutes : minutes}`;
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let day = days[date.getDay()];
+  let month = months[date.getMonth()];
+
+  return `${day} ${month} ${date.getDate()}, ${hours}:${
+    minutes < 10 ? "0" + minutes : minutes
+  }`;
 }
 
 function searchCity(city) {
-    let apiKey = "ba4608fb3t14e3e4af50o5a7601d9309";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  
-    axios.get(apiUrl).then(refreshWeather);
+  let apiKey = "ba4608fb3t14e3e4af50o5a7601d9309";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(refreshWeather);
 }
 
 function handleSearchSubmit(event) {
-    event.preventDefault();
-    let searchInput = document.querySelector("#search-form-input");
-    let cityElement = document.querySelector("#city");
-  
-    cityElement.innerHTML = searchInput.value;
-    searchCity(searchInput.value);
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#city");
+
+  cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
-searchCity("Nyazura");
-
+function displayForecast() {
+    let forecast = document.querySelector("#forecast");
   
+    let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+    let forecastHtml = "";
+  
+    days.forEach(function (day) {
+      forecastHtml =
+        forecastHtml +
+        `  <div>
+              <div class="weather-forecast-date">${day}</div>
+              <div class="weather-forecast-icon">🌦</div>
+  <div class="weather-forecast-temperatures">
+  <span class="weather-forecast-temperature-max">18°</span>
+    <span class="weather-forecast-temperature-min">12°</span>
+  </div>
+  </div>
+  
+  `;
+    });
+  
+    forecastElement.innerHTML = forecastHtml; 
+  }
+  
+  let forecastElement = document.querySelector("#forecast"); 
+  displayForecast(); 
+  searchCity("Nyazura");
+
+
+
+
