2dbb94fc4f1a1f9e36a611687d3f8b52af2f592e
Nattie14 <166996757+Nattie14@users.noreply.github.com>
2024-04-28 13:32:44 +0100

    Updated java script to start forecast from next day

--- src/script.js
+++ src/script.js
@@ -105,7 +105,7 @@ function displayForecast(response) {
     let forecastHtml = "";
   
     response.data.daily.forEach(function (day,index) {
-        if (index <5) {
+        if (index > 0 && index <= 5) {
     forecastHtml =
     forecastHtml +
 `  <div>
